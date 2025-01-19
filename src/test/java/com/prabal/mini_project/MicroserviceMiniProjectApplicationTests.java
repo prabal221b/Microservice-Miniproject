8e4d7786f3fce27e29cc2b2660b0d0772cc1abac
prabal <imprabalpratap@gmail.com>
2025-01-19 13:30:17 -0700

    First commit, adding springboot starter code with dependencies

---
+++ src/test/java/com/prabal/mini_project/MicroserviceMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prabal.mini_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceMiniProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
